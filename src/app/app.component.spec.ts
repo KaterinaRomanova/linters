a1322cfe72aec5a28ad3bab97bcc440618d20e95
KaterinaRomanova <44569848+KaterinaRomanova@users.noreply.github.com>
2021-08-18 15:46:40 +0300

    fix:test2

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -27,7 +27,7 @@ describe('AppComponent', () => {
     fixture.detectChanges();
     const compiled = fixture.nativeElement;
     expect(compiled.querySelector('.content span').textContent).toContain(
-      'info-navek-soft app is running!'
+      'info-navek-soft app is running!',
     );
   });
 });
